f74afd842c584d5e9a0e929976cc32e9589c4a87
Anoop <anooprav7@gmail.com>
2019-08-21 00:41:20 +0530

    docs(docz): props docs added and basic notes

---
+++ src/docs/docsIconPropTypes/WrapperForStyledSvg.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StyledSvg } from '../../components/StyledSvg';
+
+const WrapperForStyledSvg = props => <StyledSvg {...props} />;
+
+// https://css-tricks.com/accessible-svgs/
+WrapperForStyledSvg.propTypes = {
+	/** Provides a human-readable title for the element that contains it. If not provided, will be the Icon name. [W3Org](https://www.w3.org/TR/SVG-access/#Equivalent) */
+	a11yTitle: PropTypes.string.isRequired,
+	/**  A valid theme color value. Eg. primary500, secondary400  */
+	themeColor: PropTypes.oneOf(['primary', 'secondary', 'action', 'error', 'disabled']),
+	/** Applies a color attribute to the SVG element. Takes precedence over themeColor */
+	htmlColor: PropTypes.string,
+	/** Size of the Icon. Same scale as font scale in stormbreaker theme token   */
+	size: PropTypes.string
+};
+WrapperForStyledSvg.defaultProps = {
+	size: 'medium'
+};
+
+export default WrapperForStyledSvg;
